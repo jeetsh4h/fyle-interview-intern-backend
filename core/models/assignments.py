55547423275e07a8d40779b5f0c15c985805fb12
jeetsh4h <jeetsh4h@gmail.com>
2024-08-05 20:07:11 +0530

    Add principal/assignments/* APIs

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -72,11 +72,27 @@ class Assignment(db.Model):
         return assignment
 
 
+    # TODO: bug fix ~ teacher_id check
+    # TODO: bug fix ~ state check
     @classmethod
-    def mark_grade(cls, _id, grade, auth_principal: AuthPrincipal):
+    def mark_grade_by_teacher(cls, _id, grade, auth_principal: AuthPrincipal):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
+
+        assignment.grade = grade
+        assignment.state = AssignmentStateEnum.GRADED
+        db.session.flush()
+
+        return assignment
+    
+    @classmethod
+    def mark_grade_by_principal(cls, _id, grade, auth_principal: AuthPrincipal):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
+
+        assertions.assert_valid(cls.state != AssignmentStateEnum.DRAFT, 'draft assignments cannot be graded')
 
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
@@ -88,6 +104,14 @@ class Assignment(db.Model):
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
 
+    # TODO: bug fix ~ teacher_id check
     @classmethod
     def get_assignments_by_teacher(cls):
         return cls.query.all()
+    
+    @classmethod
+    def get_assignments_by_principal(cls):
+        return cls.filter(
+            cls.state.in_(
+                [AssignmentStateEnum.SUBMITTED, AssignmentStateEnum.GRADED]
+            )).all()
